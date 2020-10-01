cdb976bb84e8ad1c3f316b129bc15c91a66bc486
Machonvic <hongovictor@gmail.com>
2020-10-01 13:33:13 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -86,13 +86,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id=item.getItemId();
-
-        if(id==R.id.info){
-            Intent infoIntent  =new Intent(this, InfoActivity.class);
-            startActivity(infoIntent );
-
-        }
+            Intent intent  =new Intent(this, InfoActivity.class);
+            startActivity(intent );
         return super.onOptionsItemSelected(item);
         }
 
